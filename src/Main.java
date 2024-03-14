1b66b143a7ddeb438e59053473283a8e5285a160
Kazedesu1 <111736493+Kazedesu1@users.noreply.github.com>
2024-03-14 08:46:57 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.io.*;
+
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            BufferedReader fin = new BufferedReader(new FileReader("input.txt"));
+            StringBuilder str = new StringBuilder();
+            String line;
+                while ((line = fin.readLine()) != null) {
+                    str.append(line).append("\n");
+                }
+            fin.close();
 
-
-        Bài làm viết tại đây
-
+            String replacedContent = str.toString().replaceAll("Nha Trang", "Vũng Tàu");
+            BufferedWriter fout = new BufferedWriter(new FileWriter("output.txt"));
+            fout.write(replacedContent);
+            fout.close();
+        } catch (IOException e) {
+            System.out.println("Đã xảy ra lỗi: " + e.getMessage());
+        }
     }
 }
